62b4edfbcfe5121b8a500348274f55da82005532
John Doe <adamalfraih@gmail.com>
2022-09-25 19:34:32 +0200

    recognise when a key is pressed on the midi keyboard. midi command, midi number/key, and velocity is tracked. The key and velocity is now console logged

--- app.js
+++ app.js
@@ -3,12 +3,41 @@ if (navigator.requestMIDIAccess) {
 }
 
 function success(midiAccess) {
-    console.log(midiAccess);
     midiAccess.addEventListener('statechange', updateDevices)
+
+    const inputs = midiAccess.inputs;
+
+    inputs.forEach((input) => {
+        input.addEventListener('midimessage', handleInput)
+    })
 }
 
+function handleInput(input) {
+    const command = input.data[0];
+    const note = input.data[1];
+    const velocity = input.data[2];
+
+    switch (command) {
+        case 144: // noteOn 
+        if (velocity > 0) {
+            noteOn(note, velocity);
+        } else {
+            noteOff(note);
+        }
+        break;
+        case 128: // note off
+        noteOff(note);
+        break;
+    }
+}
+
+function noteOn(note, velocity) {
+    console.log(note, velocity)
+}
+function noteOff(note, velocity) {
+    console.log(note)
+}
 function updateDevices(event) {
-console.log(event)
 console.log(`Device ${event.port.state}. Name: ${event.port.name}, Brand: ${event.port.manufacturer}, Type: ${event.port.type}`)
 }
 
