734ac9a638bb53fef8446abb585fe59eef9bf67e
John Doe <adamalfraih@gmail.com>
2022-09-25 18:37:48 +0200

    event listener added. console log message when device is disconnected / connected

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ if (navigator.requestMIDIAccess) {
 
 function success(midiAccess) {
     console.log(midiAccess);
-    midiAccess.onstatechange = updateDevices;
+    midiAccess.addEventListener('statechange', updateDevices)
 }
 
 function updateDevices(event) {
 console.log(event)
+console.log(`Device ${event.port.state}. Name: ${event.port.name}, Brand: ${event.port.manufacturer}, Type: ${event.port.type}`)
 }
 
 function failure() {
